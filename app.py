d019de1a3ed018e70515be277813002709586272
Lân Phan <93138988+itzL0g4n@users.noreply.github.com>
2026-02-06 18:25:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ async def get_console_script():
         return {"content": "// Could not load file: " + str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True, access_log=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, access_log=False)
+
